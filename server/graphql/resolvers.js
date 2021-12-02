d97adeb4f5489292090eb8f0f5e7d0bf089b15da
Alejandra Vidal <alejavida@gmail.com>
2021-12-02 17:54:25 -0500

    actualiza adiciona resolver y tipos en inscripcion

--- server/graphql/resolvers.js
+++ server/graphql/resolvers.js
@@ -1,5 +1,6 @@
 const resolversProyectos = require('../models/proyecto/resolvers');
 const resolversUsuarios = require('../models/usuario/resolvers');
+const resolversInscripcion = require('../models/inscripcion/resolvers');
 
-const resolvers = [resolversProyectos, resolversUsuarios];
+const resolvers = [resolversProyectos, resolversUsuarios, resolversInscripcion];
 module.exports = resolvers;
--- server/models/inscripcion/Inscripcion.js
+++ server/models/inscripcion/Inscripcion.js
@@ -1,31 +1,30 @@
 const { Schema, model } = require('mongoose');
 const { Enum_FaseProyecto, Enum_EstadoProyecto, Enum_EstadoInscripcion } = require('../enums');
 
-const ProyectoSchema = Schema({
-  
-    idProyecto: {
-        type: Schema.Types.ObjectId,
-        ref: 'Proyecto',
-        required: true,
-    },  
+const InscripcionSchema = Schema({  
+  idProyecto: {
+    type: Schema.Types.ObjectId,
+    ref: 'Proyecto',
+    required: true,
+  },  
 
-    idEstudiante: {
+  idEstudiante: {
     type: Schema.Types.ObjectId,
     ref: 'Usuario',
     required: true,
     },
-    estadoInscripcion: {
+   estadoInscripcion: {
     type: String,
     required: true,
     trim: true,
     enum: Enum_EstadoInscripcion,
     default: Enum_EstadoInscripcion.PENDIENTE,
-  },
-    fechaIngreso: {
+   },
+  fechaIngreso: {
     type: Date,
     required: true,
     trim: true,
-  },
+   },
   fechaEgreso: {
     type: Date,
     required: true,
