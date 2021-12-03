b81ea01280f7dc8732e3b7675e8f73d58f56b271
Eliab Lopez <eliablopez@hotmail.com>
2021-12-02 20:16:22 -0500

    Importa tipos de inscripcion al archivo typeDefs

--- server/graphql/schema.js
+++ server/graphql/schema.js
@@ -1,6 +1,7 @@
 const tiposUsuario = require('../models/usuario/tipos');
 const tiposProyecto = require('../models/proyecto/tipos');
 const tiposAvance = require('../models/avance/tipos');
+const tiposInscripcion = require('../models/inscripcion/tipos');
 
-const typeDefs = [tiposUsuario, tiposProyecto, tiposAvance];
+const typeDefs = [tiposUsuario, tiposProyecto, tiposAvance, tiposInscripcion];
 module.exports = typeDefs;
